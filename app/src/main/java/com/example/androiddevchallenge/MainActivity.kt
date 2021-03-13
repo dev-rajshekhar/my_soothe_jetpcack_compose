8dcca31b5c2c304ff3b47f176d859a55c44c0149
Rajshekhar Yadav <yrajshekhar231@gmail.com>
2021-03-13 23:13:47 +0530

    Fixed Bugs

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
 
-            MyTheme(darkTheme = false) {
+            MyTheme() {
                 NavGraph(window)
             }
         }
